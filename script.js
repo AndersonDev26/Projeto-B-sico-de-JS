ae08f8ed2b0adeaf7bc5081e696f39e4359490c4
AndersonDev26 <andsilv14@gmail.com>
2026-04-15 01:08:50 -0300

    commit full

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+function adicionarTarefa() {
+   let mensagem = "Tarefa adicionada com sucesso!";
+   let mensagemerro = "Erro!"
+
+
+
+   let inputtarefa = document.getElementById("inputtarefa")
+   let tarefa = inputtarefa.value.trim()
+
+
+
+   if (tarefa == "") {
+      document.getElementById("mensagem").textContent = mensagemerro;
+   } else {
+      document.getElementById("mensagem").textContent =  mensagem;
+      let listatarefas = document.getElementById("listatarefas")
+      let novatarefa = document.createElement("li")
+      novatarefa.textContent = tarefa
+      listatarefas.appendChild(novatarefa)
+   }
+
+
+   
+
+
+   inputtarefa.value = ""
+
+
+
+}
+
